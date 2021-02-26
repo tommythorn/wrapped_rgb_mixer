985908da4de8401d5dedcc0778b297ffbb927006
Tommy Thorn <tommy-github2@thorn.ws>
2021-02-26 01:23:53 -0800

    Added properties and passed the test

--- properties.v
+++ properties.v
@@ -1,8 +1,18 @@
 always @(*) begin
-    if(active) begin
-        // active properties here
+    if (active) begin
+       // active properties here
+       assert(wbs_ack_o  == buf_wbs_ack_o);
+       assert(wbs_dat_o  == buf_wbs_dat_o);
+       assert(la_data_out== buf_la_data_out);
+       assert(io_out     == buf_io_out);
+       assert(io_oeb     == buf_io_oeb);
     end
-    if(!active) begin
-        // inactive properties here
+    if (!active) begin
+       // inactive properties here
+       assert(wbs_ack_o  == 0);
+       assert(wbs_dat_o  == 0);
+       assert(la_data_out== 0);
+       assert(io_out     == 0);
+       assert(io_oeb     == 0);
     end
 end
